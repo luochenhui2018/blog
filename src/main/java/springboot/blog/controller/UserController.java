35b2ac2cc7757ea109e8bf7cdc29554d4ec9af20
lch <848383@qq.com>
2018-12-19 00:30:52 +0800

    新增根据id查询用户、获取表达接口

--- src/main/java/springboot/blog/controller/UserController.java
+++ src/main/java/springboot/blog/controller/UserController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import springboot.blog.domain.User;
 import springboot.blog.repository.UserRepository;
 
 /**
@@ -31,4 +33,34 @@ public class UserController {
         model.addAttribute("title","用户管理");
         return new ModelAndView("users/list","userModel",model);
     }
+
+
+    /**
+     * 根据id查询用户
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("{id}")
+    public ModelAndView view(@PathVariable("id") long id,Model model){
+        User user = userRepository.getUserById(id);
+        model.addAttribute("user",user);
+        model.addAttribute("title","查看用户");
+        return new ModelAndView("users/view","userModel",model);
+    }
+
+    /**
+     * 获取创建表单页面
+     * @param model
+     * @return
+     */
+    @GetMapping("/form")
+    public ModelAndView createForm(Model model){
+        model.addAttribute("user",new User());
+        model.addAttribute("title","创建用户");
+        return new ModelAndView("users/form","userModel",model);
+    }
+
+
+
 }
